b9f3816920659cbeed861956661947aa62c757c4
Ricardo Montañez <rykkychikito@gmail.com>
2025-10-07 21:58:15 -0600

    Segundo commit

--- app_cliente/views.py
+++ app_cliente/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+# app_cliente/views.py
 
-# Create your views here.
+from django.shortcuts import render
+# IMPORTA TU MODELO CLIENTE de la app actual (app_cliente.models)
+from .models import Cliente 
+
 def index(request):
-    return render(request, 'index.html')
+    # 1. Obtiene TODOS los objetos de tu modelo Cliente
+    datos_cliente = Cliente.objects.all()
+    
+    contexto = {
+        # Cambiamos el nombre de la variable para ser más claros, si quieres
+        'clientes': datos_cliente  
+    }
+    
+    # Renderiza la plantilla, pasando el contexto
+    return render(request, 'index.html', contexto)
